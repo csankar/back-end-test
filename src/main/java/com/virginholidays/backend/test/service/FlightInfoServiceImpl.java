e7eb20652481357476f4180580dffdb492bcb09f
csankar <chandransankar@googlemail.com>
2023-03-12 21:52:45 +0000

    Test Virgin Atlantic ~ Flight Information Display

--- src/main/java/com/virginholidays/backend/test/service/FlightInfoServiceImpl.java
+++ src/main/java/com/virginholidays/backend/test/service/FlightInfoServiceImpl.java
@@ -2,11 +2,14 @@ package com.virginholidays.backend.test.service;
 
 import com.virginholidays.backend.test.api.Flight;
 import com.virginholidays.backend.test.repository.FlightInfoRepository;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
-import org.springframework.stereotype.Service;
+import java.util.stream.Collectors;
 
 /**
  * The service implementation of FlightInfoService
@@ -16,21 +19,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class FlightInfoServiceImpl implements FlightInfoService {
 
-    private final FlightInfoRepository flightInfoRepository;
+  private final FlightInfoRepository flightInfoRepository;
 
-    /**
-     * The constructor
-     *
-     * @param flightInfoRepository the flightInfoRepository
-     */
-    public FlightInfoServiceImpl(FlightInfoRepository flightInfoRepository) {
-        this.flightInfoRepository = flightInfoRepository;
-    }
+  /**
+   * The constructor
+   *
+   * @param flightInfoRepository the flightInfoRepository
+   */
+  public FlightInfoServiceImpl(FlightInfoRepository flightInfoRepository) {
+    this.flightInfoRepository = flightInfoRepository;
+  }
 
-    @Override
-    public CompletionStage<Optional<List<Flight>>> findFlightByDate(LocalDate outboundDate) {
+  @Override
+  public CompletionStage<Optional<List<Flight>>> findFlightByDate(LocalDate outboundDate) {
 
-        // FIXME - applicant to complete
-        return flightInfoRepository.findAll();
-    }
+    // FIXME - applicant to complete
+    CompletionStage<Optional<List<Flight>>> allFlights = flightInfoRepository.findAll();
+    return allFlights.thenApplyAsync(flights -> {
+      return Optional.of(
+              flights.get()
+                      .stream()
+                      .filter(flight ->
+                              flight
+                                      .days()
+                                      .contains(outboundDate.getDayOfWeek())
+                      ).collect(Collectors.toList())
+                      .stream()
+                      .sorted(Comparator.comparing(Flight::departureTime))
+                      .collect(Collectors.toList()));
+    });
+  }
 }
